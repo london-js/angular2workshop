d0932f5e02378918c6c03982fad4a74e8cad3c36
Alex Lakatos <alex.lakatos@ovoenergy.com>
2016-06-25 21:32:20 +0100

    adding headers to make swapi work in firefox

--- app/components/shared/base-client.service.ts
+++ app/components/shared/base-client.service.ts
@@ -1,16 +1,18 @@
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class BaseClient{
     baseUrl:string;
-    
+
     constructor(private http:Http) {
         this.baseUrl = 'http://swapi.co/api';
     }
-    
+
     public get(url:string):Observable<any> {
-        return this.http.get(`${this.baseUrl}${url}`);
+        let headers = new Headers({ 'Accept': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.get(`${this.baseUrl}${url}`, options);
     }
 }
